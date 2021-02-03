196d836db49fafbcb645e61cdf26811af2f77bf8
Rui Liu <ruiliu310@gmail.com>
2021-02-02 19:34:32 -0500

    add no_beta version

---
+++ pr_script_no_ab.sh
@@ -0,0 +1,8 @@
+python compute_pr_no_beta.py -p 0.9
+python compute_pr_no_beta.py -p 0.85
+python compute_pr_no_beta.py -p 0.7
+python compute_pr_no_beta.py -p 0.5
+python compute_pr_no_beta.py -p 0.3
+python compute_pr_no_beta.py -p 0.1
+
+wait;
